b2880ccac9a00e4c55185de78568c5df93105ff6
sye1101 <shinye1101@naver.com>
2023-09-25 00:01:26 +0900

    230925 commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,16 +15,22 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        //1번 호출
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        //1번 호출
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        //1번 호출
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
